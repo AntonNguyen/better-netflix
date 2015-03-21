f6fe0a485dee387bbc35d7447344ed41aa556651
Anton Nguyen <anton@freshbooks.com>
2015-03-21 09:38:26 -0400

    Get rid of the annoying hover scrolling on netflix

---
+++ content_script.js
@@ -0,0 +1,17 @@
+_.map(
+  _.toArray(document.getElementsByClassName('slider')),
+  function(e) {
+    e.parentNode.replaceChild(e.firstChild, e)
+  }
+);
+_.map(
+  _.union(
+    _.toArray(document.getElementsByClassName('sliderButton')),
+    _.toArray(document.getElementsByClassName('evidence')),
+    _.toArray(document.getElementsByClassName('sharing-prompt')),
+    _.toArray(document.getElementsByClassName('boxShotDivider'))
+  ),
+  function(e) {
+    e.parentNode.removeChild(e)
+  }
+);
